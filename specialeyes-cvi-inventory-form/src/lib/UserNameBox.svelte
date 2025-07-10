9efb11e8cff82838c917d00ad5d049426c7ddcd4
SmartBaby626 <addytwhite@icloud.com>
2025-07-10 11:24:02 +1000

    Changed TextBox.svelte to UserNameBox.svelte and made it bindable

---
+++ specialeyes-cvi-inventory-form/src/lib/UserNameBox.svelte
@@ -0,0 +1,36 @@
+<script>
+  export let value = '';
+</script>
+
+<input
+  type="text"
+  name="text"
+  class="input"
+  placeholder="Enter your name"
+  bind:value
+/>
+
+<style>
+  .input {
+    font-family: -apple-system,BlinkMacSystemFont,"Segoe UI","Roboto","Oxygen","Ubuntu","Cantarell","Fira Sans","Droid Sans","Helvetica Neue",sans-serif;
+    font-weight: 500;
+    font-size: .8vw;
+    color: #530A7A;
+    background-color: rgb(255,255,255);
+    box-shadow: 0 0 .4vw rgba(0,0,0,0.5), 0 0 0 .15vw transparent;
+    border-radius: 0.4vw;
+    border: none;
+    outline: none;
+    padding: 0.4vw;
+    max-width: 190px;
+    transition: .4s;
+  }
+
+  .input:hover {
+    box-shadow: 0 0 0 .15vw #530A7A80;
+  }
+
+  .input:focus {
+    box-shadow: 0 0 0 .15vw #530A7A;
+  }
+</style>
