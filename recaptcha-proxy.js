c368105b8cc390701816877d20785d60a2dc117f
SmartBaby626 <addytwhite@icloud.com>
2025-07-19 13:47:58 +1000

    Updates

--- recaptcha-proxy.js
+++ recaptcha-proxy.js
@@ -1,1 +1,5 @@
-document.write('<script src="https://www.google.com/recaptcha/api.js?render=6LdoDn8rAAAAAAKejpFmQdqT0A0p1C3IzPUlJ4iZ"><\/script>');
+const script = document.createElement('script');
+script.src = 'https://www.google.com/recaptcha/api.js?render=6LdoDn8rAAAAAAKejpFmQdqT0A0p1C3IzPUlJ4iZ';
+script.async = true;
+script.defer = true;
+document.head.appendChild(script);
--- recaptcha-v2-proxy.js
+++ recaptcha-v2-proxy.js
@@ -1,1 +1,5 @@
-document.write('<script src="https://www.google.com/recaptcha/api.js?onload=onRecaptchaV2LoadCallback&render=explicit"><\/script>');
+const script = document.createElement('script');
+script.src = 'https://www.google.com/recaptcha/api.js?onload=onRecaptchaV2LoadCallback&render=explicit';
+script.async = true;
+script.defer = true;
+document.head.appendChild(script);
