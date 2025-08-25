4ab827de1e033f8362a02122dc6d0d4217f8dcf4
SmartBaby626 <addytwhite@icloud.com>
2025-08-26 06:54:05 +1000

    Remove unused blobToBase64 function to clean up code

--- specialeyes-cvi-inventory-form/src/App.svelte
+++ specialeyes-cvi-inventory-form/src/App.svelte
@@ -193,14 +193,6 @@
     handleDynamicSubmit();
   };
   
-  async function blobToBase64(blob) {
-    return new Promise((resolve, reject) => {
-      const reader = new FileReader();
-      reader.onloadend = () => resolve(reader.result);
-      reader.onerror = reject;
-      reader.readAsDataURL(blob);
-    });
-  }
 
   function selectAge(group) {
     ageGroup = group;
